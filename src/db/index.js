d3662c8215aa8d2adebd819a24ea45b41e02932d
Abhijit <abhijitmoharana70@gmail.com>
2026-06-26 20:17:38 +0530

    db correction

--- src/db/index.js
+++ src/db/index.js
@@ -3,14 +3,17 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(`${process.env.mongodb_url}/${DB_NAME}`)
-    console.log(`\n mongoDB connected !! DB HOST:${connectionInstance.Connection.host}`);
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
 
+    console.log(
+      `MongoDB Connected!! DB HOST: ${connectionInstance.connection.host}`
+    );
   } catch (error) {
-    console.log("mongoDB connection Error", error);
-    process.exit(1)
-
+    console.error("MongoDB Connection Error:", error);
+    process.exit(1);
   }
-}
+};
 
 export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 //require('dotenv').config({path:'./env'}) //use this or best methord is use below things
-
+console.log(process.env.MONGODB_URI);
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
